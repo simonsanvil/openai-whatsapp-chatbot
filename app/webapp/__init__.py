227083fa1ed518e2e489842043b350faa3540ecf
Simon Sanchez V <simonsanvil2012@gmai.com>
2021-08-25 14:22:53 +0200

    changed port number to 8000

--- app/webapp/__init__.py
+++ app/webapp/__init__.py
@@ -1,4 +1,4 @@
 from .webchat import app
 import os
 def main():
-     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
