2e7419cbaee08aec6f2c1481b6413428cac29ef9
Simon S Viloria <simonsviloria@gmail.com>
2022-12-15 00:38:36 +0000

    removed redundant files and refractoring

--- app/whatsapp/__init__.py
+++ app/whatsapp/__init__.py
@@ -1,9 +1,10 @@
-from .whatsapp_app import app, send_whatsapp_message
+from .app import app, send_whatsapp_message
 import os,sys
 
 __all__ = ['app','send_whatsapp_message']
 
-sys.path.append('whatsapp-app')
 def main():
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
-    # app.run()
+
+if __name__=='__main__':
+    app.run(debug=False)
--- app/whatsapp/__main__.py
+++ app/whatsapp/__main__.py
@@ -1,3 +1,9 @@
-from .whatsapp_app import app
+from .app import app
+import os
+
+def main():
+    # start the app
+    app.run(debug=False, host=os.environ.get('APP_HOST', '0.0.0.0'), port=int(os.environ.get('APP_PORT', 8000)))
+
 if __name__=='__main__':
-    app.run(debug=False)
+    main()
